65621dc3ad47146c864f929ce6cbe504d86a32d1
giperxiog <1512739072@qq.com>
2024-04-15 02:08:36 +0800

    test ign

--- config/Account.js
+++ config/Account.js
@@ -3,8 +3,10 @@ module.exports = {
     BASE_URL:'mongodb://127.0.0.1/lvyou-project',
 
     // 阿里云短信验证服务
-    ALI_KEYID: '填写自己的',
-    ALI_KEYSECRET: '填写自己的',
+    // ALI_KEYID: '填写自己的',
+    // ALI_KEYSECRET: '填写自己的',
+    ALI_KEYID: 'LTAI4tN6yfwaxw6iB3gKeNpw',
+    ALI_KEYSECRET: 'vQ2c922S2XgtPlmZhdXGJDibLCygmy',
     ALI_SIGNNAME: '旅游攻略平台小程序',
     ALI_CODE: 'SMS_465434299',
     
