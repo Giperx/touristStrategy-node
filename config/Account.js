ec71c6a0c185490312baf9d209142f57bbe56eda
giperxiog <1512739072@qq.com>
2024-04-15 02:06:31 +0800

    remove key

--- config/Account.js
+++ config/Account.js
@@ -3,8 +3,8 @@ module.exports = {
     BASE_URL:'mongodb://127.0.0.1/lvyou-project',
 
     // 阿里云短信验证服务
-    ALI_KEYID: 'LTAI5tN6yfwaxw6iB3gKeNpw',
-    ALI_KEYSECRET: 'vQ3c922S2XgtPlmZhdXGJDibLCygmy',
+    ALI_KEYID: '填写自己的',
+    ALI_KEYSECRET: '填写自己的',
     ALI_SIGNNAME: '旅游攻略平台小程序',
     ALI_CODE: 'SMS_465434299',
     
